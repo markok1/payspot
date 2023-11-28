a3e93a0999c4a7fb940ac3a71386343f1a182e4c
markok1 <markogit123@gmail.com>
2023-11-28 17:38:55 +0100

    izmjene

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-$(".navbar .for_mobile ul .dropdown_li_sm a").click(function (e) {
+$(".navbar .for_mobile ul .dropdown_li_sm > a").click(function (e) {
   if ($(window).width() < 900) {
     if (!$(this).parent().parent().parent().hasClass("ford")) {
       e.preventDefault();
@@ -45,8 +45,38 @@ $(".navbar .for_mobile ul .dropdown_li_sm a").click(function (e) {
         $(this).removeClass("dropdown_active");
         $(this).next("").css("max-height", "0px");
       } else {
+        $(".dropdown_active").next().css("max-height", "0px");
+        $(".dropdown_active").removeClass("dropdown_active");
         $(this).addClass("dropdown_active");
         var getheight = $(this).next().find(".boxes").height() + 50;
+        var submenuheight =
+          $(this).next().find(".extended-boxes").height() + 10;
+        var totalheight = 0;
+        if (isNaN(submenuheight)) {
+          totalheight = getheight;
+        } else {
+          totalheight = getheight + submenuheight;
+        }
+        $(this)
+          .next("")
+          .css("max-height", totalheight + "px");
+      }
+    }
+  }
+});
+$(".sub-menu a").click(function (e) {
+  if ($(window).width() < 900) {
+    if (!$(this).parent().parent().parent().hasClass("extended-ford")) {
+      e.preventDefault();
+
+      if ($(this).hasClass("extended-dropdown-active")) {
+        $(this).removeClass("extended-dropdown-active");
+        $(this).next("").css("max-height", "0px");
+      } else {
+        $(".extended-dropdown-active").next().css("max-height", "0px");
+        $(".extended-dropdown-active").removeClass("extended-dropdown-active");
+        $(this).addClass("extended-dropdown-active");
+        var getheight = $(this).next().find(".extended-boxes").height() + 50;
         $(this)
           .next("")
           .css("max-height", getheight + "px");
@@ -56,6 +86,11 @@ $(".navbar .for_mobile ul .dropdown_li_sm a").click(function (e) {
 });
 $(".uskoro").click(function (e) {
   e.preventDefault();
+});
+$(".language-picker a").click(function (e) {
+  if ($(window).width() > 900) {
+    e.preventDefault();
+  }
 });
 
 $(".root").scroll(function () {
@@ -92,7 +127,7 @@ $(".about-us-slider").slick({
   slidesToShow: 3,
   adaptiveHeight: true,
   autoplay: true,
-  autoplaySpeed: 3000,
+  autoplaySpeed: 2500,
   responsive: [
     {
       breakpoint: 770,
