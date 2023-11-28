148b0c0e8991e743b2dedc5511301e4385e72fdf
markok1 <markogit123@gmail.com>
2023-11-28 17:48:40 +0100

    izmjene2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show-fade");
-    }
-  });
-});
-
-const hiddenElements = document.querySelectorAll(".hidden-fade");
-hiddenElements.forEach((el) => observer.observe(el));
-
 // js for navbar
 $(".navbar .lines").click(function (e) {
   e.preventDefault();
